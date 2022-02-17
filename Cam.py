1c5932fdf003fba7f5be370a509a17396f01cd39
VJ9X <69524036+VJ9X@users.noreply.github.com>
2022-02-17 08:20:22 -0600

    Create Cam.py

---
+++ Cam.py
@@ -0,0 +1,176 @@
+import pygame
+
+
+
+class whatcam:
+    def __init__(self, c1a, c1b, c1c, c2a, c2b, c3, c4a, c4b, c5, c6, c7):
+        self.c1a = c1a
+        self.c1b = c1b
+        self.c1c = c1c
+        self.c2a = c2a
+        self.c2b = c2b
+        self.c3 = c3
+        self.c4a = c4a
+        self.c4b = c4b
+        self.c5 = c5
+        self.c6 = c6
+        self.c7 = c7
+    
+    def one_A(self):
+        
+        self.c1a = True
+        self.c1b = False
+        self.c1c = False
+        self.c2a = False
+        self.c2b = False
+        self.c3 = False
+        self.c4a = False
+        self.c4b = False
+        self.c5 = False
+        self.c6 = False
+        self.c7 = False
+        
+    
+    def one_B(self):
+        
+        self.c1a = False
+        self.c1b = True
+        self.c1c = False
+        self.c2a = False
+        self.c2b = False
+        self.c3 = False
+        self.c4a = False
+        self.c4b = False
+        self.c5 = False
+        self.c6 = False
+        self.c7 = False
+    
+    def one_C(self):
+        
+        self.c1a = False
+        self.c1b = False
+        self.c1c = True
+        self.c2a = False
+        self.c2b = False
+        self.c3 = False
+        self.c4a = False
+        self.c4b = False
+        self.c5 = False
+        self.c6 = False
+        self.c7 = False
+    
+    def two_A(self):
+        
+        self.c1a = False
+        self.c1b = False
+        self.c1c = False
+        self.c2a = True
+        self.c2b = False
+        self.c3 = False
+        self.c4a = False
+        self.c4b = False
+        self.c5 = False
+        self.c6 = False
+        self.c7 = False
+    
+    def two_B(self):
+        
+        self.c1a = False
+        self.c1b = False
+        self.c1c = False
+        self.c2a = False
+        self.c2b = True
+        self.c3 = False
+        self.c4a = False
+        self.c4b = False
+        self.c5 = False
+        self.c6 = False
+        self.c7 = False
+        
+    def three(self):
+        
+        self.c1a = False
+        self.c1b = False
+        self.c1c = False
+        self.c2a = False
+        self.c2b = False
+        self.c3 = True
+        self.c4a = False
+        self.c4b = False
+        self.c5 = False
+        self.c6 = False
+        self.c7 = False
+        
+    def four_A(self):
+        
+        self.c1a = False
+        self.c1b = False
+        self.c1c = False
+        self.c2a = False
+        self.c2b = False
+        self.c3 = False
+        self.c4a = True
+        self.c4b = False
+        self.c5 = False
+        self.c6 = False
+        self.c7 = False
+        
+    def four_B(self):
+        
+        self.c1a = False
+        self.c1b = False
+        self.c1c = False
+        self.c2a = False
+        self.c2b = False
+        self.c3 = False
+        self.c4a = False
+        self.c4b = True
+        self.c5 = False
+        self.c6 = False
+        self.c7 = False
+        
+    def five(self):
+        
+        self.c1a = False
+        self.c1b = False
+        self.c1c = False
+        self.c2a = False
+        self.c2b = False
+        self.c3 = False
+        self.c4a = False
+        self.c4b = False
+        self.c5 = True
+        self.c6 = False
+        self.c7 = False
+        
+    def six(self):
+        
+        self.c1a = False
+        self.c1b = False
+        self.c1c = False
+        self.c2a = False
+        self.c2b = False
+        self.c3 = False
+        self.c4a = False
+        self.c4b = False
+        self.c5 = False
+        self.c6 = True
+        self.c7 = False
+        
+    def seven(self):
+        
+        self.c1a = False
+        self.c1b = False
+        self.c1c = False
+        self.c2a = False
+        self.c2b = False
+        self.c3 = False
+        self.c4a = False
+        self.c4b = False
+        self.c5 = False
+        self.c6 = False
+        self.c7 = True
+    
+    
+        
+        
